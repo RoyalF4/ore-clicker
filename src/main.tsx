a1c2b61f213691608169ff45d829c5af2866d143
Royal Finch <Royal2791@gmail.com>
2024-05-29 20:07:12 -0400

    feat: :sparkles: add routing for home/app/stats/error pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Start Screen</div>,
+  },
+  {
+    path: "/app",
+    element: <App />,
+  },
+  {
+    path: "/stats",
+    element: <div>Stats</div>,
+  },
+  {
+    path: "*",
+    element: <div>Error</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
